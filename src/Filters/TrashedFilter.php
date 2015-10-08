394e80c08c9723fc729e0b32720211ba72097820
Pablo Martelletti <pmartelletti@gmail.com>
2015-10-08 17:13:58 -0300

    Remove time-aware filter for softDeletes.

--- src/Filters/TrashedFilter.php
+++ src/Filters/TrashedFilter.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Query\Filter\SQLFilter;
 class TrashedFilter extends SQLFilter {
 
     public function addFilterConstraint(ClassMetadata $metadata, $table) {
-        return $this->isSoftDeletable($metadata->rootEntityName) ? "{$table}.deleted_at IS NULL OR CURRENT_TIMESTAMP < {$table}.deleted_at" : '';
+        return $this->isSoftDeletable($metadata->rootEntityName) ? "{$table}.deleted_at IS NULL" : '';
     }
 
     private function isSoftDeletable($entity) {
